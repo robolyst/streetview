372480594fa6190d093422061bb42ea076a372a2
Sunny Singh <sunny@doeity.com>
2020-10-07 18:43:44 +0530

    Update __init__.py

--- streetview/__init__.py
+++ streetview/__init__.py
@@ -220,7 +220,7 @@ def api_download(panoid, heading, flat_dir, key, width=640, height=640,
             camera. You need to specify a direction in degrees as the photo
             will only cover a partial region of the panorama. The recommended
             headings to use are 0, 90, 180, or 270.
-        :flat_dir: the direction to save the image to.
+        :flat_dir: the directory to save the image to (eg, C:\Users\SUNNY\Documents).
         :key: your API key.
         :width: downloaded image width (max 640 for non-premium downloads).
         :height: downloaded image height (max 640 for non-premium downloads).
