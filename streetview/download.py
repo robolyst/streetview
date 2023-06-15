f43a0916deb0ac1efdb52d2babfbbfc6a53801d4
Matan Abudy <matanabudy@users.noreply.github.com>
2023-06-15 10:42:40 +0300

    Add zoom parameter to download panoramas (#26)

--- streetview/download.py
+++ streetview/download.py
@@ -22,43 +22,55 @@ class Tile:
     image: Image.Image
 
 
-def iter_tile_info(pano_id: str) -> Generator[TileInfo, None, None]:
+def get_width_and_height_from_zoom(zoom: int) -> (int, int):
+    """
+    Returns the width and height of a panorama at a given zoom level, depends on the zoom level.
+    """
+    return 2**zoom, 2 ** (zoom - 1)
+
+
+def make_download_url(pano_id: str, zoom: int, x: int, y: int) -> str:
+    """
+    Returns the URL to download a tile.
+    """
+    return f"https://cbk0.google.com/cbk?output=tile&panoid={pano_id}&zoom={zoom}&x={x}&y={y}"
+
+
+def fetch_panorama_tile(tile_info: TileInfo) -> Image.Image:
+    """
+    Tries to download a tile, returns a PIL Image.
+    """
+    while True:
+        try:
+            response = requests.get(tile_info.fileurl, stream=True)
+            break
+        except requests.ConnectionError:
+            print("Connection error. Trying again in 2 seconds.")
+            time.sleep(2)
+
+    return Image.open(BytesIO(response.content))
+
+
+def iter_tile_info(pano_id: str, zoom: int) -> Generator[TileInfo, None, None]:
     """
     Generate a list of a panorama's tiles and their position.
     """
-
-    image_url = (
-        "https://cbk0.google.com/cbk?output=tile&panoid={0:}&zoom=5&x={1:}&y={2:}"
-    )
-
-    for x, y in itertools.product(range(26), range(13)):
+    width, height = get_width_and_height_from_zoom(zoom)
+    for x, y in itertools.product(range(width), range(height)):
         yield TileInfo(
             x=x,
             y=y,
-            fileurl=image_url.format(pano_id, x, y),
+            fileurl=make_download_url(pano_id=pano_id, zoom=zoom, x=x, y=y),
         )
 
 
-def iter_tiles(pano_id: str) -> Generator[Tile, None, None]:
-    for info in iter_tile_info(pano_id):
-        # Try to download the image file
-        while True:
-            try:
-                response = requests.get(info.fileurl, stream=True)
-                break
-            except requests.ConnectionError:
-                print("Connection error. Trying again in 2 seconds.")
-                time.sleep(2)
-
-        image = Image.open(BytesIO(response.content))
-
+def iter_tiles(pano_id: str, zoom: int) -> Generator[Tile, None, None]:
+    for info in iter_tile_info(pano_id, zoom):
+        image = fetch_panorama_tile(info)
         yield Tile(x=info.x, y=info.y, image=image)
 
-        del response
-        del image
 
-
-def get_panorama(pano_id: str) -> Image.Image:
+def get_panorama(pano_id: str, zoom: int = 5) -> Image.Image:
     """
     Downloads a streetview panorama.
     """
@@ -66,9 +78,10 @@ def get_panorama(pano_id: str) -> Image.Image:
     tile_width = 512
     tile_height = 512
 
-    panorama = Image.new("RGB", (26 * tile_width, 13 * tile_height))
+    total_width, total_height = get_width_and_height_from_zoom(zoom)
+    panorama = Image.new("RGB", (total_width * tile_width, total_height * tile_height))
 
-    for tile in iter_tiles(pano_id=pano_id):
+    for tile in iter_tiles(pano_id=pano_id, zoom=zoom):
         panorama.paste(im=tile.image, box=(tile.x * tile_width, tile.y * tile_height))
         del tile
 
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,23 +1,83 @@
+import hashlib
+from io import BytesIO
+
 import pytest
+from PIL import Image
 
 from streetview import get_panorama
-from streetview.download import iter_tile_info, iter_tiles
+from streetview.download import (
+    TileInfo,
+    fetch_panorama_tile,
+    get_width_and_height_from_zoom,
+    iter_tile_info,
+    iter_tiles,
+    make_download_url,
+)
+
+# This MD5 was retrieved empirically by downloading tile with bad coordinates
+BAD_TILE_MD5 = "be32aa9ed3880664433199f9e0615cd6"
 
 
-def test_that_all_the_tiles_are_generated():
-    tiles = list(iter_tile_info(pano_id="z80QZ1_QgCbYwj7RrmlS0Q"))
-    assert len(tiles) == 26 * 13
+def hash_tile(tile: Image.Image):
+    """
+    Returns the md5 hash of a tile
+    """
+    tile_mock_file = BytesIO()
+    tile.save(tile_mock_file, "jpeg")
+    tile_mock_file.seek(0)
+    return hashlib.md5(tile_mock_file.read()).hexdigest()
+
+
+@pytest.mark.parametrize("zoom", [1, 2, 3, 4, 5, 6, 7])
+@pytest.mark.vcr()
+def test_width_from_zoom_is_correct(zoom: int):
+    width, height = get_width_and_height_from_zoom(zoom)
+
+    # Try to fetch one more tile than the width and height and verify we get a bad tile
+    out_of_bound_width_tile_info = TileInfo(
+        x=width + 1,
+        y=height,
+        fileurl=make_download_url(
+            pano_id="z80QZ1_QgCbYwj7RrmlS0Q", zoom=zoom, x=width + 1, y=height
+        ),
+    )
+    out_of_bound_tile = fetch_panorama_tile(out_of_bound_width_tile_info)
+    assert hash_tile(out_of_bound_tile) == BAD_TILE_MD5
+
+
+@pytest.mark.parametrize("zoom", [1, 2, 3, 4, 5, 6, 7])
+@pytest.mark.vcr()
+def test_height_from_zoom_is_correct(zoom: int):
+    width, height = get_width_and_height_from_zoom(zoom)
+
+    # Try to fetch one more tile than the width and height and verify we get a bad tile
+    out_of_bound_height_tile_info = TileInfo(
+        x=width,
+        y=height + 1,
+        fileurl=make_download_url(
+            pano_id="z80QZ1_QgCbYwj7RrmlS0Q", zoom=zoom, x=width, y=height + 1
+        ),
+    )
+    out_of_bound_tile = fetch_panorama_tile(out_of_bound_height_tile_info)
+    assert hash_tile(out_of_bound_tile) == BAD_TILE_MD5
+
+
+@pytest.mark.parametrize("zoom", [1, 2, 3, 4, 5, 6, 7])
+def test_that_all_the_tiles_are_generated(zoom: int):
+    tiles = list(iter_tile_info(pano_id="z80QZ1_QgCbYwj7RrmlS0Q", zoom=zoom))
+    width, height = get_width_and_height_from_zoom(zoom)
+    assert len(tiles) == width * height
 
 
 @pytest.mark.vcr()
 def test_that_first_tile_can_be_saved():
-    tiles = iter_tiles(pano_id="z80QZ1_QgCbYwj7RrmlS0Q")
+    tiles = iter_tiles(pano_id="z80QZ1_QgCbYwj7RrmlS0Q", zoom=1)
     tile = next(tiles)
     tile.image.save("image.jpg", "jpeg")
     next(tiles)
 
 
-@pytest.mark.skip(reason="Takes a really long time.")
+@pytest.mark.vcr()
 def test_that_panorama_downloads_successfully():
-    image = get_panorama(pano_id="z80QZ1_QgCbYwj7RrmlS0Q")
+    image = get_panorama(pano_id="z80QZ1_QgCbYwj7RrmlS0Q", zoom=1)
     image.save("image.jpg", "jpeg")
