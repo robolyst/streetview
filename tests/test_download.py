ec1e4605177bee67489dd5eafe74e7be153aeebf
Yufeng Zhao <yufeng-zhao@outlook.com>
2024-06-07 00:10:17 +0200

    add crop test

--- tests/test_download.py
+++ tests/test_download.py
@@ -95,3 +95,13 @@ def test_that_panorama_downloads_successfully_multi_threaded():
 async def test_that_panorama_downloads_successfully_async():
     image = await get_panorama_async(pano_id="z80QZ1_QgCbYwj7RrmlS0Q", zoom=1)
     image.save("image.jpg", "jpeg")
+
+
+@pytest.mark.vcr()
+def test_that_panorama_downloads_successfully_crop_bottom_right_border():
+    # this pano_id has a black border on the bottom right
+    pano_id = "EVGmA-L6LuI_7-elZaDq1g"
+    for i in range(1, 6):
+        image = get_panorama(pano_id=pano_id, zoom=i)
+        image = crop_bottom_and_right_black_border(image)
+        image.save(f"image-z{i}.jpg", "jpeg")
